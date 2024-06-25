a48649c2f77ab5d0f5568690e4345e78ee8086bb
chenDesktop <975817692@qq.com>
2024-06-25 21:38:41 +0800

    Step3

--- TestProject/3.c
+++ TestProject/3.c
@@ -2,22 +2,41 @@
 #include <math.h>
 #define M_PI 3.14159265358979323846 
 
-int main()
+int getNumSpaces(float sinVal)
+{
+    float numSpaces;
+    numSpaces = 30 + sinVal * 30;
+    return numSpaces;
+
+}
+
+
+
+
+int step3()
 {
     double angle;
     int numSteps = 20;
+    scanf_s("%d", &numSteps);
     double maxAngle = M_PI * 2;
     int i;
     double sinVal;
 
 
     for (i = 0; i <= numSteps; i++)
-    {
+    {   
+        int k = 0;
         angle = (double)i / (double)numSteps * maxAngle;
         sinVal = sin(angle);
-        printf("%3d: %5.2f %6.3f\n", i, angle, sinVal);
-
+        printf("%3d: %5.2f %6.3f ", i, angle, sinVal);
+        k = getNumSpaces(sinVal);
+        while (k > 0)
+        {
+            printf(" ");
+            k--;
+        }
+        printf("*\n");
+       
     }
 }
-
 

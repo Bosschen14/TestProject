9122d10bb5760c28d9d9498a47ba3b5771817846
Chen1@Hl.com <Chen1@HL.com>
2024-06-25 23:28:01 +0800

    123

---
+++ TestProject/moon.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+/*
+ * Simple lunar lander program.
+ * By:  <insert name here>
+ */
+
+
+double altitude = 100;  /* Meters */
+double velocity = 0;    /* Meters per second */
+double fuel = 100;      /* Kilograms */
+double power = 1.5;     /* Acceleration per pound of fuel */
+double g = -1.63;       /* Moon gravity in m/s^2 */
+double burn;            /* Amount of fuel to burn */
+
+int main()
+{
+    printf("Lunar Lander - (c) 2012, by <insert name here>\n");
+
+}

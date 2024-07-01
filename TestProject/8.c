82e5f97f1014b94e40aad1707a65341a83e0d2fe
chenDesktop <975817692@qq.com>
2024-07-01 16:59:54 +0800

    strcpy真的很有用

--- TestProject/8.c
+++ TestProject/8.c
@@ -11,41 +11,51 @@
 #define MaxGrosses 5
 
 
-int highestNum(double gross[], char* names[])
+int highestNum(double gross[], char* names[],double *highestGross, char* highestName)
 {
 	int highestGrossIndex = 0;
-	double highestGross = 0;
 
 
 	for (int i = 0; i < NumMovies; i++)
 	{
 		//printf("gross[%d]:%.0lf ", i, gross[i]);
 		//printf("names[%d]:%s\n", i, names[i]);
-		if (gross[i] > highestGross)
+		if (gross[i] > *highestGross)
 		{
 			//printf("i%d", i);
 			highestGrossIndex = i;
-			highestGross = gross[highestGrossIndex];
+			*highestGross = gross[highestGrossIndex];
 		}
 		//printf("highestGrossIndex%d\n", highestGrossIndex);
 	}
 	//printf("names[highestGrossIndex]%s", names[highestGrossIndex]);
-	if (names[highestGrossIndex] == "")
+	if (strcmp(names[highestGrossIndex], "") == 0)
 	{
-		printf("No movies\n");
+		printf("No movies ");
 	}
 	else
 	{
-		printf("film is: %s\n", names[highestGrossIndex]);
+		printf("Film is: %s ", names[highestGrossIndex]);
+		strcpy(highestName, names[highestGrossIndex]);/*strcpy***********/
+
 	}
+
 	
-	
+	highestGross = &gross[highestGrossIndex];
+
+	//printf("highestGros01s%.0lf\n", *highestGross);
+	//printf("highestGros02s %s\n", highestName);
 	return 0;
 }
 
 
 int main()
 {
+	double a = 0;
+	char b[100] = "";
+	double* highestGross = &a;
+	char* highestName = &b;
+
 	double gross[NumMovies] = { 4633070, 3856195, 3171189, 1877685, 3251622,
 							   3156594, 1629735, 2659234, 2028036, 510768 };
 	char *names[NumMovies] = { "Hall Pass", "Unknown", "I Am Number Four", "The King's Speech",
@@ -59,7 +69,7 @@ int main()
 		printf("Movie %2d %33s: %.0f\n", i + 1, names[i], gross[i]);
 	}
 	printf("The highest gross  is ");
-	highestNum(gross,names);
+	highestNum(gross,names, highestGross, highestName);
 
 
 
@@ -89,9 +99,14 @@ int main()
 
 			}
 		}
+
+		*highestGross = 0; // Reset highestGross for new comparison
 		printf("The highest gross less than %.0lf is ", maxGross[i]);
-		highestNum(remainingGross, remainingNames);
-		//printf("at %.0lf");
+		highestNum(remainingGross, remainingNames, highestGross, highestName);
+
+		//printf("highestGros03s%.0lf\n", *highestGross);
+		//printf("highestGros04s%s\n", highestName);
+		printf("at %.0lf\n" , *highestGross);
 
 	}
 }

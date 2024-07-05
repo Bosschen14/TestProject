72191dc842f822ac0b95935221d06edd917c3e59
chenDesktop <975817692@qq.com>
2024-07-05 13:16:34 +0800

    ss

--- TestProject/9.c
+++ TestProject/9.c
@@ -1,43 +1,58 @@
-#include <stdio.h>
-
-/*
- * Name : <Insert name>
- * Program to experiment with character arrays
- */
-
-#define MaxWord 20
-
-int main()
-{
-    int len = 0;
-    char c;
-    char str[MaxWord + 1];
-
-    puts("Enter text. Include a dot ('.') to end a sentence to exit:");
-    do
-    {
-        c = getchar();
-
-        if (c != ' ' && c != '.' && len < MaxWord)
-        {
-            /* This is a character of a word */
-            str[len] = c;
-            len++;
-
-        }
-        else if (c = '  ')
-        {
-            printf("str is  ");
-        }
-        else
-        {
-            /* The word is done */
-           /* The word is done */
-            str[len] = 0;
-            printf("%s\n", str);
-            len = 0;
-        }
-    } while (c != '.');
-
-
-}
+//#include <stdio.h>
+//
+///*
+// * Name : <Insert name>
+// * Program to experiment with character arrays
+// */
+//
+//#define MaxWord 20
+//
+//int main()
+//{
+//    int len = 0;
+//    char c;
+//    char str[MaxWord + 1];
+//    int sum = 0;
+//    int numberStr = 0;
+//    int maxlen = 0;
+//    puts("Enter text. Include a dot ('.') to end a sentence to exit:");
+//    do
+//    {
+//        c = getchar();
+//
+//        if (c != ' ' && c != '.' && len < MaxWord)
+//        {
+//            /* This is a character of a word */
+//            str[len] = c;
+//            len++;
+//
+//        }
+//
+//        else
+//        {
+//            /* The word is done */
+//           /* The word is done */
+//            str[len] = 0;
+//            printf("%d\n", len);
+//            //printf("%s\n", str);
+//            if (maxlen < len)
+//            {
+//                maxlen = len; 
+//            }
+//            
+//
+//            numberStr++;
+//            //printf("%d\n", numberStr);
+//            sum += len;
+//            //printf("%d\n", sum);
+//
+//            len = 0;
+//
+//        }
+//    } while (c != '.');
+//    double average = 0;
+//    average = sum / (numberStr);
+//    printf("average%.0lf\n", average);
+//    printf("maxlen: %d\n", maxlen);
+//
+//}
